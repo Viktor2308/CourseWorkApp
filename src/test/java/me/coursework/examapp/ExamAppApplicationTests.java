9955e68858ebd5c23fde21295a1e9db695c829ed
Viktor <viktor_23_08@mail.ru>
2023-02-16 15:37:46 +0100

    initial commit

---
+++ src/test/java/me/coursework/examapp/ExamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.coursework.examapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
